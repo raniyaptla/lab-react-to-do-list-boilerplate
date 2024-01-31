0087d4060579fadf6acf8f14815c613dfaab5765
raniyaptla <ra.niyapoonthala@gmail.com>
2024-01-31 23:15:18 +0530

    updt

---
+++ todo/src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  // add a new todo
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: newTodo }]);
+      setNewTodo('');
+    }
+  };
+
+  // update a todo
+  const updateTodo = (id, newText) => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === id ? { ...todo, text: newText } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  // delete a todo
+  const deleteTodo = id => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="container">
+      <h1>To-Do List</h1>
+
+      
+      <div className="input-container">
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add new todo"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+
+      {/* Display Todos */}
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id} className="todo-item">
+            <input
+              type="text"
+              value={todo.text}
+              onChange={(e) => updateTodo(todo.id, e.target.value)}
+            />
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
